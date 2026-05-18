318343d0256f54e26fbe6e678f192a2c0f2327e6
Fovir <fovir@disroot.org>
2026-05-18 23:32:18 +0800

    chore: provide a test of transform excel into sqlite

--- struct_excel/main.py
+++ struct_excel/main.py
@@ -1,5 +1,4 @@
-from dataclasses import fields, is_dataclass
-from enum import Enum
+from struct_excel.database import init_db, model_to_db
 from struct_excel.normalization import normalize_sheet
 from openpyxl.reader.excel import load_workbook
 from openpyxl.workbook import Workbook
@@ -14,35 +13,6 @@ from struct_excel.transform import (
 import logging
 import argparse
 from pathlib import Path
-
-
-def dataclass_to_row(obj):
-    row = []
-    for f in fields(obj):
-        value = getattr(obj, f.name)
-        if isinstance(value, Enum):
-            value = value.value
-        row.append(value)
-
-    return row
-
-
-def write_dataclass_sheet(workbook: Workbook, sheet_name: str, data: list):
-    ws = workbook.create_sheet(title=sheet_name)
-
-    if not data:
-        return
-
-    if not is_dataclass(data[0]):
-        raise ValueError(f"{sheet_name} contains non-dataclass objects")
-
-    # Title
-    headers = [f.name for f in fields(data[0])]
-    ws.append(headers)
-
-    # Data
-    for obj in data:
-        ws.append(dataclass_to_row(obj))
 
 
 def parse_args():
@@ -62,7 +32,10 @@ def main():
     dist_dir = Path("dist")
     dist_dir.mkdir(parents=True, exist_ok=True)
 
+    DB_PATH = "sqlite:///./dist/test.db"
     ERR_XLSX = "./dist/err.xlsx"
+
+    engine = init_db(DB_PATH)
 
     args = parse_args()
 
@@ -83,10 +56,6 @@ def main():
     err_wb.save(ERR_XLSX)
 
     for sheet in wb:
-        output = Workbook()
-        output.remove(output.active)  # pyright: ignore
-        output_dir = f"dist/{sheet.title}_output.xlsx"
-
         try:
             raw_rows = read_raw_row(sheet)
         except ValueError as e:
@@ -99,13 +68,11 @@ def main():
         sessions = to_session(raw_rows, courses)
         enrollments = to_enrollment(raw_rows, students, courses, sessions)
 
-        write_dataclass_sheet(output, "supervisors", supervisors)
-        write_dataclass_sheet(output, "courses", courses)
-        write_dataclass_sheet(output, "students", students)
-        write_dataclass_sheet(output, "sessions", sessions)
-        write_dataclass_sheet(output, "enrollments", enrollments)
-        output.save(output_dir)
-        print(f"Saved to {output_dir}, and errors saved to {ERR_XLSX}")
+        model_to_db(engine, supervisors)
+        model_to_db(engine, courses)
+        model_to_db(engine, students)
+        model_to_db(engine, sessions)
+        model_to_db(engine, enrollments)
 
 
 if __name__ == "__main__":
