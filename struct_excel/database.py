e28ab7ccda933edd31ee3fc3012e1bfb59a5130b
Fovir <fovir@disroot.org>
2026-05-18 23:24:03 +0800

    feat: add `model_to_db()` function

--- struct_excel/database.py
+++ struct_excel/database.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Engine
+from sqlmodel import Session
+from sqlalchemy import Engine
 from sqlmodel import SQLModel, create_engine
 
 
@@ -6,3 +8,9 @@ def init_db(path: str) -> Engine:
     engine: Engine = create_engine(path)
     SQLModel.metadata.create_all(engine)
     return engine
+
+
+def model_to_db(engine: Engine, models):
+    with Session(engine) as session:
+        session.add_all(models)
+        session.commit()
